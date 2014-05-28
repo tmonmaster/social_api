cd59df86dbb74dfd71eb93732b8381adac8f0685
u_kino07 <u_kino07@tmon-u_kino07.tmoncorp.com>
2014-05-28 10:21:47 +0900

    [NEWOJT-73][강이경][뉴스리스트 객체를 XML로 출력]
    NewsController.java 생성
    
    News 객체를 JAXB로 Marshalling 한 결과 Stream을 String으로 반환하는 기능을 가진 클래스

--- social-api/src/main/java/kr/co/tmon/social/api/controller/NewsController.java
+++ social-api/src/main/java/kr/co/tmon/social/api/controller/NewsController.java
@@ -1,25 +1,75 @@
 package kr.co.tmon.social.api.controller;
 
-import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 
 import kr.co.tmon.social.api.service.NewsService;
+import kr.co.tmon.social.api.vo.News;
+import kr.co.tmon.social.api.vo.RootNews;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * @author raspilla16@tmon.co.kr
+ * News 객체를 JAXB로 Marshalling 한 결과 Stream을 String으로 반환하는 기능을 가진 클래스
+ * 
+ * @author 강이경
  * 
  */
 @Controller
 public class NewsController {
 
+	private static JAXBContext CONTEXT;
+
+	public NewsController() throws JAXBException {
+		CONTEXT = JAXBContext.newInstance(RootNews.class);
+	}
+
 	@Autowired
 	private NewsService newsService;
 
+	public void setNewsService(NewsService newsService) {
+		this.newsService = newsService;
+	} 
+
 	@RequestMapping("/news.xml")
-	public void printNewsListWithJaxb(HttpServletResponse response) throws Exception {
-		newsService.printJaxbNewsList(response.getOutputStream());
+	@ResponseBody
+	/**
+	 * Marshalling한 결과 XML 을 string으로 return 하는 메소드
+	 * 
+	 * @return String
+	 * @throws Exception
+	 */
+	public String printNewsListWithJaxb() throws Exception {
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		List<News> newsList = newsService.getNewsList();
+		createNewsApiUsingJAXB(outputStream, newsList);
+		return outputStream.toString();
 	}
+	
+	/**
+	 * 
+	 * 뉴스 객체를 JAXB로 Marshalling하는 메소드
+	 * 
+	 * @param outputStream
+	 * @param newsList
+	 * @throws JAXBException
+	 * @throws PropertyException
+	 */
+	private void createNewsApiUsingJAXB(ByteArrayOutputStream outputStream, List<News> newsList) throws JAXBException, PropertyException {
+		RootNews rootNews = new RootNews();
+		rootNews.setNewsList(newsList);
+
+		Marshaller marshaller = CONTEXT.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.marshal(rootNews, outputStream);
+	}
+
 }
