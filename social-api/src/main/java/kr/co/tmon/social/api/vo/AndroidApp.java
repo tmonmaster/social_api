a6ddee077313445de4d0387ca219f41bbf793913
정승현 <raspilla16@tmon.co.kr>
2014-05-28 14:41:46 +0900

    [NEWOJT-87] [안드로이드 앱 리뷰 제공 API 개발] [정승현]
    VO 객체 추가
    
    AndroidApp
    
    AndroidAppReview

---
+++ social-api/src/main/java/kr/co/tmon/social/api/vo/AndroidApp.java
@@ -0,0 +1,31 @@
+package kr.co.tmon.social.api.vo;
+
+/**
+ * @author raspilla16@tmon.co.kr
+ * 
+ */
+public class AndroidApp {
+	private String androidAppId;
+	private String companyId;
+
+	public String getAndroidAppId() {
+		return androidAppId;
+	}
+
+	public void setAndroidAppId(String androidAppId) {
+		this.androidAppId = androidAppId;
+	}
+
+	public String getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+
+	@Override
+	public String toString() {
+		return "AndroidApp [androidAppId=" + androidAppId + ", companyId=" + companyId + "]";
+	}
+}
