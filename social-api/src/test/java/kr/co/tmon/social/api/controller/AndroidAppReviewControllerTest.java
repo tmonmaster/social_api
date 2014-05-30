34ed281bc13bad2900b07269aeef77765a9a59e6
u_kino07 <u_kino07@tmon-u_kino07.tmoncorp.com>
2014-05-30 14:57:40 +0900

    [NEWOJT-94][강이경][리뷰 제공 controller 구현]
    AndroidAppReviewController.java 생성
    
    AndroidAppReview 객체를 JSON으로 변환한 결과 Stream을 String으로 반환하는 클래스

---
+++ social-api/src/test/java/kr/co/tmon/social/api/controller/AndroidAppReviewControllerTest.java
@@ -0,0 +1,42 @@
+package kr.co.tmon.social.api.controller;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import kr.co.tmon.social.api.service.AndroidAppReviewService;
+import kr.co.tmon.social.api.vo.AndroidAppReview;
+
+/**
+ * AppReviewContoller.getAppReviewListUsingJSON 테스트
+ * 
+ * @author 강이경
+ * 
+ */
+public class AndroidAppReviewControllerTest {
+	@Test
+	public void getAndroidAppReviewListUsingJSON() {
+		AndroidAppReviewController androidAppReviewController = new AndroidAppReviewController();
+		AndroidAppReviewService androidAppReviewService = mock(AndroidAppReviewService.class);
+		androidAppReviewController.setAndroidAppReviewService(androidAppReviewService);
+
+		List<AndroidAppReview> androidAppReviewList = new ArrayList<AndroidAppReview>();
+		androidAppReviewList.add(new AndroidAppReview(
+				"https://play.google.com/store/apps/details?id=com.tmon&reviewId=Z3A6QU9xcFRPR2tpTTJQQUlDZXRUM21hOHg0VHRCNkIyTVVwMW5QT1E3VWRXRGRpSEM1VzBYMTJlaU9Bc01fRHR6U2dKRzNoZXUyNnNkS0pRN3FVVE5FTHc", 
+				"정혜경", 
+				"https://play.google.com/store/people/details?id=111359932959764103963", 
+				"2014년 5월 28일", 
+				"20%", 
+				"안하고말지요",
+				"무슨문제인지는 모르겟지만 검색후에 아래로 내리면 다시 티몬 로그인 동그라미 화면나오고 이래서 머 하겠어요 다신안함", "티켓몬스터"));
+
+		when(androidAppReviewService.getAndroidAppReviewList()).thenReturn(androidAppReviewList);
+
+		String jsonString = androidAppReviewController.getAndroidAppReviewListUsingJSON(androidAppReviewList);
+		System.out.println(jsonString);
+	}
+}
