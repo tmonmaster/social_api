70cf8fbf089325bb4956e7a400eefc803600495f
Yi-kyung <u_kino07@naver.com>
2014-06-03 02:38:44 +0900

    [NEWOJT-94][강이경][리뷰 제공 Controller 구현]
    Controller 테스트 (AndroidAppReviewControllerTest.java)
    
    - Mockito 사용해 목처리 부분 수정
      @RunWith, @Mock, @InjectMocks 추가
    - Test 코드 수정
      기존의 테스트 코드에서는 androidAppReviewList를 Json으로 변환한 결과를 출력하기만 해서, JUnit
    테스트를 실행하면 문제가 생겨도 알아낼 수 없었다. 따라서 기존코드에 assertNotNull을 추가하였다.
    AndroidAppReviewService에서 androidAppReviewList가 null인지 체크 할 수 있도록 수정하였다.

--- social-api/src/test/java/kr/co/tmon/social/api/controller/AndroidAppReviewControllerTest.java
+++ social-api/src/test/java/kr/co/tmon/social/api/controller/AndroidAppReviewControllerTest.java
@@ -1,45 +1,55 @@
 package kr.co.tmon.social.api.controller;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Test;
-
 import kr.co.tmon.social.api.service.AndroidAppReviewService;
 import kr.co.tmon.social.api.vo.AndroidAppReview;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
 /**
- * AndroidAppReviewContoller.getAndroidAppReviewListUsingJSON 테스트
+ * AndroidAppReviewService에서 androidAppReviewList를 받아와서 Controller에서 Json으로 변환하는 테스트 클래스
  * 
  * @author 강이경
  * 
  */
+@RunWith(MockitoJUnitRunner.class)
 public class AndroidAppReviewControllerTest {
+	@Mock
+	private AndroidAppReviewService androidAppReviewService;
+
+	@InjectMocks
+	private AndroidAppReviewController androidAppReviewController;
+
+	List<AndroidAppReview> androidAppReviewList;
+	
+	@Before
+	public void androidAppReviewList_세팅(){
+		androidAppReviewList = new ArrayList<AndroidAppReview>();
+		androidAppReviewList	.add(new AndroidAppReview(
+						"https://play.google.com/store/apps/details?id=com.tmon&reviewId=Z3A6QU9xcFRPR2tpTTJQQUlDZXRUM21hOHg0VHRCNkIyTVVwMW5QT1E3VWRXRGRpSEM1VzBYMTJlaU9Bc01fRHR6U2dKRzNoZXUyNnNkS0pRN3FVVE5FTHc",
+						"정혜경",
+						"https://play.google.com/store/people/details?id=111359932959764103963",
+						"2014년 5월 28일",
+						"20%",
+						"안하고말지요",
+						"무슨문제인지는 모르겟지만 검색후에 아래로 내리면 다시 티몬 로그인 동그라미 화면나오고 이래서 머 하겠어요 다신안함",
+						"티켓몬스터", "tmonplus", "3.3.1"));
+	     when(androidAppReviewService.getAndroidAppReviewList()).thenReturn(androidAppReviewList);
+	}
+	
 	@Test
-	public void getAndroidAppReviewListUsingJSON() {
-		AndroidAppReviewController androidAppReviewController = new AndroidAppReviewController();
-		AndroidAppReviewService androidAppReviewService = mock(AndroidAppReviewService.class);
-		androidAppReviewController.setAndroidAppReviewService(androidAppReviewService);
-
-		List<AndroidAppReview> androidAppReviewList = new ArrayList<AndroidAppReview>();
-		androidAppReviewList.add(new AndroidAppReview(
-				"https://play.google.com/store/apps/details?id=com.tmon&reviewId=Z3A6QU9xcFRPR2tpTTJQQUlDZXRUM21hOHg0VHRCNkIyTVVwMW5QT1E3VWRXRGRpSEM1VzBYMTJlaU9Bc01fRHR6U2dKRzNoZXUyNnNkS0pRN3FVVE5FTHc", 
-				"정혜경", 
-				"https://play.google.com/store/people/details?id=111359932959764103963", 
-				"2014년 5월 28일", 
-				"20%", 
-				"안하고말지요",
-				"무슨문제인지는 모르겟지만 검색후에 아래로 내리면 다시 티몬 로그인 동그라미 화면나오고 이래서 머 하겠어요 다신안함", 
-				"티켓몬스터",
-				"tmonplus",
-				"3.3.1"));
-
-		when(androidAppReviewService.getAndroidAppReviewList()).thenReturn(androidAppReviewList);
-
-		String jsonString = androidAppReviewController.getAndroidAppReviewListUsingJSON(androidAppReviewList);
-		System.out.println(jsonString);
+	public void getAndroidAppReviewList() {
+		assertNotNull(androidAppReviewList);
+		System.out.println(androidAppReviewController.getJsonForAndroidAppReviewList());
 	}
 }
