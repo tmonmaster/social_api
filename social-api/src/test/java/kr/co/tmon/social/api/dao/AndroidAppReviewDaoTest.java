4f724a0a9afcbb4cf206dad9416e0ae616680658
Yi-kyung <u_kino07@naver.com>
2014-06-09 05:35:45 +0900

    [NEWOJT-95][강이경][리뷰를 DB에서 가져오는 Service 및 Dao 구현]
    AndroidAppReviewDaoTest.java
    Dao 테스트 클래스 수정
    
    - Dao에 startDate, endDate를 받아와 그 사이에 해당하는 날짜의 AppReviewList를 DB에서 가져오는
    기능 반영
    - 그에 따른 테스트 코드 수정

--- social-api/src/test/java/kr/co/tmon/social/api/dao/AndroidAppReviewDaoTest.java
+++ social-api/src/test/java/kr/co/tmon/social/api/dao/AndroidAppReviewDaoTest.java
@@ -1,23 +1,35 @@
 package kr.co.tmon.social.api.dao;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.text.ParseException;
+import java.util.List;
+
 import kr.co.tmon.social.api.vo.AndroidAppReview;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * AndroidAppReviewDao 테스트 클래스
  * 
  * @author 강이경
- *
+ * 
  */
 public class AndroidAppReviewDaoTest {
 	@Test
-	public void _AndroidAppReviewDao_테스트() {
+	public void AndroidAppReviewDao_테스트() throws ParseException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/applicationContext.xml", "classpath:spring/dispatcher-servlet.xml");
 		AndroidAppReviewDao androidAppReviewDao = (AndroidAppReviewDao) context.getBean("androidAppReviewDao");
 
-		for (AndroidAppReview androidAppReview : androidAppReviewDao.getAndroidAppReviewList())
-			System.out.println(androidAppReview.toString());
+		List<AndroidAppReview> androidAppReviewList = androidAppReviewDao.selectAndroidAppReviewListBetween("2014-05-26", "2014-05-28");
+		assertNotNull(androidAppReviewList);
+
+		for (AndroidAppReview androidAppReview : androidAppReviewList) {
+			String reviewDate = androidAppReview.getReviewDate();
+			assertTrue(reviewDate.equals("2014-05-26") || reviewDate.equals("2014-05-27") || reviewDate.equals("2014-05-28"));
+		}
 	}
 }
