7fa56b2c2961d8f49e92399525e023b03c74cab2
Yi-kyung <u_kino07@naver.com>
2014-06-09 05:41:19 +0900

    [NEWOJT-95][강이경][리뷰를 DB에서 가져오는 Service 및 Dao 구현]
    AndroidAppReviewDao.java 수정
    
    - Service로 부터 넘겨받은 startDate, endDate 파라미터에 사이에 해당하는 앱 리뷰만 DB에서 가져오도록 추가
    기능 구현
    - (1) 파라미터가 없을 경우, endDate를 현재날짜로 잡고, startDate를 일주일 전으로 잡는다.
    - (2) startDate만 있을 경우, endDate를 그 이후 일주일 후로 잡는다.
    - (3) endDate만 있을 경우, startDate를 그 이전 일주일 전으로 잡는다.
    - (4) 파라미터가 둘다 존재할 경우, 각 파라미터를 startDate, endDate로 잡는다.

--- social-api/src/main/java/kr/co/tmon/social/api/dao/AndroidAppReviewDao.java
+++ social-api/src/main/java/kr/co/tmon/social/api/dao/AndroidAppReviewDao.java
@@ -1,9 +1,16 @@
 package kr.co.tmon.social.api.dao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import kr.co.tmon.social.api.vo.AndroidAppReview;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -19,8 +26,41 @@ public class AndroidAppReviewDao {
 	private static final String ANDROID_APP_REVIEW_MAPPER = "kr.co.tmon.social.api.dao.mapper.AndroidAppMapper.";
 	@Autowired
 	private SqlSession sqlSession;
-
-	public List<AndroidAppReview> getAndroidAppReviewList() {
-		return sqlSession.selectList(ANDROID_APP_REVIEW_MAPPER + "getAndroidAppReviewList");
+	
+	public List<AndroidAppReview> selectAndroidAppReviewListBetween(String startDate, String endDate) throws ParseException {
+		
+		if(StringUtils.isEmpty(startDate) && StringUtils.isNotEmpty(endDate)){
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(dateFormat.parse(endDate));
+			calendar.add(Calendar.DATE, -7);
+			startDate = dateFormat.format(calendar.getTime());
+		}
+		
+		if(StringUtils.isNotEmpty(startDate) && StringUtils.isEmpty(endDate)){
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar calendar = Calendar.getInstance();			
+			calendar.setTime(dateFormat.parse(startDate));
+			calendar.add(Calendar.DATE, 7);
+			endDate = dateFormat.format(calendar.getTime());
+		}
+		
+		if(StringUtils.isEmpty(startDate) && StringUtils.isEmpty(endDate)){
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar calendar = Calendar.getInstance();
+			startDate = dateFormat.format(calendar.getTime());
+			calendar.add(Calendar.DATE, 7);
+			endDate = dateFormat.format(calendar.getTime());
+		}
+		
+		if(StringUtils.isNotEmpty(startDate) && StringUtils.isNotEmpty(endDate)){
+			
+		}
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("startDate", startDate);
+		map.put("endDate", endDate);
+	
+		return sqlSession.selectList(ANDROID_APP_REVIEW_MAPPER + "getAndroidAppReviewList", map);
 	}
 }
